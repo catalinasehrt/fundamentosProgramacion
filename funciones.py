c0f229cbe6a6a8cdfecc9c5f12bea4ae194f0cf3
catalinasehrt <catalina.dali94@gmail.com>
2026-06-22 22:04:10 -0400

    commit3

---
+++ funciones.py
@@ -0,0 +1,13 @@
+# funciones sin argumentos y sin retorno
+
+# def funcion_sencilla(): #Funcion_sencilla(), sin argumentos y emite mensaje propio de la función
+   # print(" Funcion sencilla")
+# funcion_sencilla() #Llamada a función saludo(), no envía argumentos
+
+def suma (a,b):
+    sumar = a + b
+    print( f"la suma es : {sumar}" )
+num1 = int(input("Ingresa el primer numero"))
+num2= int(input("ingresa el segundo numero"))
+
+suma(num1,num2)
